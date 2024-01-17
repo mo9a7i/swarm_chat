e04ed4b31ef83d74bfd43cdaca5ecf448bd299c9
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2024-01-17 12:22:24 +0300

    .

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
-    <div>
-        <header class="bg-red-500 p-4 text-white">
+    <div class="min-h-screen flex flex-col">
+        <header class="bg-yellow-500 p-4 text-white rounded-b-2xl">
             This is a header
         </header>
-        <main>
+        <main class="flex-1">
             <slot />
         </main>
-        <footer class="bg-red-500 p-4 text-white">
+        <footer class="bg-yellow-500 p-4 text-white rounded-t-2xl">
             This is a footer
         </footer>
         
